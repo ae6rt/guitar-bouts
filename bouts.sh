43000277cbd40221ac7d030199e19f66703a2008
Mark Petrovic <mspetrovic@gmail.com>
2023-01-07 09:37:54 -0800

    [HEAD] init

---
+++ bouts.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -u
+
+lines=$(wc -l drills.txt | awk '{print $1}')
+
+for j in $(seq 1 5); do 
+
+   ran=$(od -vAn -N4 -t u4 < /dev/urandom) 
+   bout=$( echo $(( ran % 31 + 1)) )
+
+   i=0
+   while read line; do
+     i=$(( i + 1 ))
+     if [ $i = $bout ];  then
+       echo "$line"
+       break
+     fi
+   done < "drills.txt"
+
+done
